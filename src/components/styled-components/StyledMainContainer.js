73bd4b8c2eb865cf8e20e38ae7ea3855f561c5b3
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-28 15:04:41 +0200

    Added basic styles normalizing.

--- src/components/styled-components/StyledMainContainer.js
+++ src/components/styled-components/StyledMainContainer.js
@@ -3,9 +3,19 @@ import styled from 'styled-components'
 const MainContainer = styled.div`
     width: 100%;
     margin: 0 auto;
-    padding: 0 14px;
+    padding: 0;
     color: white;
-    background-color: black;
+    background-color: #075975;
+    
+    header {
+      color: papayawhip;
+      background-color: #084075;
+      h1 {
+        margin: 0;
+        padding: 14px 0;
+        text-align: center;
+      }
+    }
 `
 
 export default MainContainer
