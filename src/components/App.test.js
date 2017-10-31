bd5a0eceae9f7cc4ab286220f81e98744d6779ea
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 11:52:03 +0100

    Updating tests.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,11 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import { App } from './App'
+import StyledMainContainer from "./styled-components/StyledMainContainer"
+import Modes from "./Modes";
+import Game from "./Game";
+import GameControls from "./GameControls";
+import Score from "./Score";
 
 function setup() {
   let props = {
@@ -14,5 +19,25 @@ function setup() {
 describe('App component', () => {
   it('should render without crashing', () => {
     const wrapper = setup()
+  })
+
+  it('should have StyledMainContainer component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(StyledMainContainer).length).toEqual(1)
+  })
+
+  it('should have Modes component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(Modes).length).toEqual(1)
+  })
+
+  it('should have Game component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(Game).length).toEqual(1)
+  })
+
+  it('should have GameControls component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(GameControls).length).toEqual(1)
   })
 })
