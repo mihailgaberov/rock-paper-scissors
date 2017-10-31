ae2808ead32f5dcc57f2ea5e97d39aeb9c83193f
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 11:37:35 +0100

    Updates selectors tests.

--- src/selectors/game.test.js
+++ src/selectors/game.test.js
@@ -1,16 +1,25 @@
-import { saveServerChoice } from './game'
-import { fromJS, List } from 'immutable'
+import { Map, fromJS } from 'immutable'
+import { getServerChoice, saveServerChoice } from './game'
 
 describe('Game selectors', () => {
   describe('saveServerChoice', () => {
     it('should record the server choice to the state', () => {
-      const choice = List.of(['rock'])
-      const state = {
-        serverChoice: choice
+      const gameState = {
+        serverChoice: 'rock'
       }
-      const expected = fromJS(choice)
+      const state = Map()
+      const expected = Map({ serverChoice: 'rock' })
 
-      expect(saveServerChoice(state)).toEqual(expected)
+      expect(saveServerChoice(state, gameState)).toEqual(expected)
+    })
+  })
+
+  describe('getServerChoice', () => {
+    it('should return the server choice recorded to the state', () => {
+      const state = fromJS({game: { serverChoice: 'paper'}})
+      const expected = 'paper'
+
+      expect(getServerChoice(state)).toEqual(expected)
     })
   })
 })
