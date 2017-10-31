5be6966bfea139a4b62b7ada11fbd911d70f59df
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 12:47:42 +0100

    Updated the tests for Score component.

--- src/components/Score.test.js
+++ src/components/Score.test.js
@@ -1,6 +1,8 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import Score from './Score'
+import StyledScore from "./styled-components/StyledScore"
+import StyledButton from "./styled-components/StyledButton"
 
 function setup(winner, isLoading) {
   let props = {
@@ -17,5 +19,20 @@ function setup(winner, isLoading) {
 describe('Score component', () => {
   it('should render without crashing', () => {
     const wrapper = setup(1, true)
+  })
+
+  it('should have StyledScore component', () => {
+    let wrapper = setup(1, false)
+    expect(wrapper.find(StyledScore).length).toEqual(1)
+  })
+
+  it('should have a winner', () => {
+    let wrapper = setup(1, false)
+    expect(wrapper.find('span').at(0).text()).toEqual('Player WINS')
+  })
+
+  it('should have StyledButton component', () => {
+    let wrapper = setup(1, false)
+    expect(wrapper.find(StyledButton).length).toEqual(1)
   })
 })
