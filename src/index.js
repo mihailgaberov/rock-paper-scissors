c03a8cf983ba375f4984cf7f7793ab299390d2ad
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 05:14:16 +0100

    Added configured redux store.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App/>
+  </Provider>
+), document.getElementById('root'))
