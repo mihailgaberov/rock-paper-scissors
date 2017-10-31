3d37bfcd8e9bb9ba10142de7c09a89fb472d2933
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 12:02:38 +0100

    Updating tests.

--- src/components/GameControls.test.js
+++ src/components/GameControls.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import GameControls from './GameControls'
+import StyledGameControls from "./styled-components/StyledGameControls"
+import StyledWeapon from "./styled-components/StyledWeapon"
+import Weapon from "./Weapon"
 
 function setup() {
   let props = {
@@ -14,5 +17,25 @@ function setup() {
 describe('GameControls component', () => {
   it('should render without crashing', () => {
     const wrapper = setup()
+  })
+
+  it('should have StyledGameControls component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(StyledGameControls).length).toEqual(1)
+  })
+
+  it('should have label component', () => {
+    let wrapper = setup()
+    expect(wrapper.find('label').length).toEqual(1)
+  })
+
+  it('should have StyledWeapon component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(StyledWeapon).length).toEqual(1)
+  })
+
+  it('should have Weapon component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(Weapon).length).toEqual(1)
   })
 })
