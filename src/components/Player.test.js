93d08bd5bfba741d02dd765c04747d0746dfff73
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 12:09:11 +0100

    Updating tests.

--- src/components/Player.test.js
+++ src/components/Player.test.js
@@ -1,10 +1,13 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import Player from './Player'
+import StyledPlayer from "./styled-components/StyledPlayer"
+import Weapon from "./Weapon"
 
 function setup() {
   let props = {
-
+    label: 'Player',
+    weapon: 'rock'
   }
 
   return shallow(<Player {...props} />)
@@ -13,5 +16,25 @@ function setup() {
 describe('Player component', () => {
   it('should render without crashing', () => {
     const wrapper = setup()
+  })
+
+  it('should have StyledPlayer component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(StyledPlayer).length).toEqual(1)
+  })
+
+  it('should have Weapon component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(Weapon).length).toEqual(1)
+  })
+
+  it('should have a div with class name', () => {
+    let wrapper = setup()
+    expect(wrapper.find('div').at(0).props().className).toEqual('name')
+  })
+
+  it('should have a div with class score', () => {
+    let wrapper = setup()
+    expect(wrapper.find('div').at(1).props().className).toEqual('score')
   })
 })
