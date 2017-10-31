3bbab7a5d40a6dadd1734c93494e3738caaf75c0
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 12:52:04 +0100

    Updated the tests for Weapon component.

--- src/components/Weapon.test.js
+++ src/components/Weapon.test.js
@@ -1,10 +1,13 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import Weapon from './Weapon'
+import StyledWeapon from "./styled-components/StyledWeapon"
+import Loader from "./Loader";
 
-function setup() {
+function setup(loading) {
   let props = {
-
+    icon: 'rock',
+    loading: loading
   }
 
   return shallow(<Weapon {...props} />)
@@ -13,5 +16,17 @@ function setup() {
 describe('Weapon component', () => {
   it('should render without crashing', () => {
     const wrapper = setup()
+  })
+
+  it('should have a StyledWeapon component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(StyledWeapon).length).toEqual(1)
+  })
+
+  it('should have Loader component if loading', () => {
+    let wrapper = setup(true)
+    expect(wrapper.find(Loader).length).toEqual(1)
+    wrapper = setup(false)
+    expect(wrapper.find(Loader).length).toEqual(0)
   })
 })
