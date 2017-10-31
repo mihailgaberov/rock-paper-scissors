49d572e0ba6004d50fa5a9533458f279330406a8
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-31 12:00:23 +0100

    Updating tests.

--- src/components/Game.test.js
+++ src/components/Game.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Game from './Game'
+import StyledGameArea from "./styled-components/StyledGameArea"
+import Player from "./Player"
 
 function setup() {
   let props = {
@@ -14,5 +16,20 @@ function setup() {
 describe('Game component', () => {
   it('should render without crashing', () => {
     const wrapper = setup()
+  })
+
+  it('should have StyledGameArea component', () => {
+    let wrapper = setup()
+    expect(wrapper.find(StyledGameArea).length).toEqual(1)
+  })
+
+  it('should have Player component two times', () => {
+    let wrapper = setup()
+    expect(wrapper.find(Player).length).toEqual(2)
+  })
+
+  it('should have a div vs between the players', () => {
+    let wrapper = setup()
+    expect(wrapper.find('div').at(0).text()).toEqual('vs')
   })
 })
